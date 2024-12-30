28ac91151cb98f5f8bb388504583dedd2c96abf2
yifanTech <18251957101@163.com>
2024-12-30 12:48:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import configparser
 import yaml
 import time
 from datetime import datetime
+from api_service import APIService
 from data_logic import *
 
 class MainEntry:
@@ -32,7 +33,24 @@ def main():
     main_instance = MainEntry()
     main_instance.demo_test()
 
+# main init instance.
+def main_api_service():
+    try:
+        mainEntry = MainEntry()
+        mainEntry.initlog()
+        service = APIService(mainEntry.logger)
+        uvicorn.run(
+            service.app, 
+            host="0.0.0.0", 
+            port=8000, 
+            log_level="info",
+            )
+    except Exception as e:
+        print(f"Failed to start server: {e}")
+        logging.error(f"Server startup failed: {e}")
+
 if __name__ == "__main__":
+    # main_api_service()
     main()
     
 
